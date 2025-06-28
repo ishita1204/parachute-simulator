0ec3de6a94f379e1061377d5a6b0b55730f2383d
Ishita Gupta <66788413+ishita1204@users.noreply.github.com>
2025-06-29 01:16:10 +0530

    app.py

--- app.py
+++ app.py
@@ -108,6 +108,7 @@ def main():
                 max_value=1.0,
                 value=None,
                 step=0.001,
+                 format="%.3f"
                 key="time_step"
             )
             descent_angle = st.number_input(
